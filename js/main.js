9d9f325195e718db14a76f37e0a6c714e6e0e344
eunice awuni <awunieunice@gmail.com>
2024-02-12 13:54:26 +0000

    working on appy navbar

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// alert ("we are beta!!!");
+console.log("we are beta!!!");
+
+var firstName = "micheal";
+var age = 9;
+let lastName ="Hammond";
+const boilingWaterTemperature = 100;
+
+console.log(firstName);
+console.log(age);
+console.log(lastName,boilingWaterTemperature);
+let bottle =( color:'blue' )
